cea79336250156e3a41541035bf33ca3f4ee0991
Mikolaj <mikolaj.marczak@protonmail.com>
2026-09-16 00:11:50 +0200

    bigger modal

--- change.ps1
+++ change.ps1
@@ -6,40 +6,47 @@ function Select-AudioDevice {
     [object[]]$AudioDevices
   )
 
+  $font = New-Object System.Drawing.Font('Segoe UI', 12)
+
   $form = New-Object System.Windows.Forms.Form
   $form.Text = 'Data Entry Form'
-  $form.Size = New-Object System.Drawing.Size(300,200)
+  $form.Size = New-Object System.Drawing.Size(500,300)
   $form.StartPosition = 'CenterScreen'
+  $form.Font = $font
 
   $OKButton = New-Object System.Windows.Forms.Button
-  $OKButton.Location = New-Object System.Drawing.Point(75,120)
-  $OKButton.Size = New-Object System.Drawing.Size(75,23)
+  $OKButton.Location = New-Object System.Drawing.Point(260,220)
+  $OKButton.Size = New-Object System.Drawing.Size(100,35)
+  $OKButton.Font = $font
   $OKButton.Text = 'OK'
   $OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
   $form.AcceptButton = $OKButton
   $form.Controls.Add($OKButton)
 
   $CancelButton = New-Object System.Windows.Forms.Button
-  $CancelButton.Location = New-Object System.Drawing.Point(150,120)
-  $CancelButton.Size = New-Object System.Drawing.Size(75,23)
+  $CancelButton.Location = New-Object System.Drawing.Point(370,220)
+  $CancelButton.Size = New-Object System.Drawing.Size(100,35)
+  $CancelButton.Font = $font
   $CancelButton.Text = 'Cancel'
   $CancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
   $form.CancelButton = $CancelButton
   $form.Controls.Add($CancelButton)
 
   $label = New-Object System.Windows.Forms.Label
-  $label.Location = New-Object System.Drawing.Point(10,20)
-  $label.Size = New-Object System.Drawing.Size(280,20)
+  $label.Location = New-Object System.Drawing.Point(20,20)
+  $label.Size = New-Object System.Drawing.Size(440,30)
+  $label.Font = $font
   $label.Text = 'Please make a selection from the list below:'
   $form.Controls.Add($label)
 
   $listBox = New-Object System.Windows.Forms.Listbox
-  $listBox.Location = New-Object System.Drawing.Point(10,40)
-  $listBox.Size = New-Object System.Drawing.Size(260,20)
+  $listBox.Location = New-Object System.Drawing.Point(20,60)
+  $listBox.Size = New-Object System.Drawing.Size(440,160)
+  $listBox.Font = $font
 
   $listBox.SelectionMode = 'One'
   $listBox.DisplayMember = 'Name'
-  $listBox.Height = 70
+  $listBox.Height = 160
 
   foreach ($device in $AudioDevices) {
     if ($device.Type -eq "Playback") {
