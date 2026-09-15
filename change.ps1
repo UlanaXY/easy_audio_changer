377e68581a390a82ef7d8ad19ed12b2ae46616c4
Mikolaj <mikolaj.marczak@protonmail.com>
2026-09-16 00:07:02 +0200

    modal moved to function

--- change.ps1
+++ change.ps1
@@ -1,57 +1,68 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
-$form = New-Object System.Windows.Forms.Form
-$form.Text = 'Data Entry Form'
-$form.Size = New-Object System.Drawing.Size(300,200)
-$form.StartPosition = 'CenterScreen'
+function Select-AudioDevice {
+  param(
+    [object[]]$AudioDevices
+  )
 
-$OKButton = New-Object System.Windows.Forms.Button
-$OKButton.Location = New-Object System.Drawing.Point(75,120)
-$OKButton.Size = New-Object System.Drawing.Size(75,23)
-$OKButton.Text = 'OK'
-$OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
-$form.AcceptButton = $OKButton
-$form.Controls.Add($OKButton)
+  $form = New-Object System.Windows.Forms.Form
+  $form.Text = 'Data Entry Form'
+  $form.Size = New-Object System.Drawing.Size(300,200)
+  $form.StartPosition = 'CenterScreen'
 
-$CancelButton = New-Object System.Windows.Forms.Button
-$CancelButton.Location = New-Object System.Drawing.Point(150,120)
-$CancelButton.Size = New-Object System.Drawing.Size(75,23)
-$CancelButton.Text = 'Cancel'
-$CancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
-$form.CancelButton = $CancelButton
-$form.Controls.Add($CancelButton)
+  $OKButton = New-Object System.Windows.Forms.Button
+  $OKButton.Location = New-Object System.Drawing.Point(75,120)
+  $OKButton.Size = New-Object System.Drawing.Size(75,23)
+  $OKButton.Text = 'OK'
+  $OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+  $form.AcceptButton = $OKButton
+  $form.Controls.Add($OKButton)
 
-$label = New-Object System.Windows.Forms.Label
-$label.Location = New-Object System.Drawing.Point(10,20)
-$label.Size = New-Object System.Drawing.Size(280,20)
-$label.Text = 'Please make a selection from the list below:'
-$form.Controls.Add($label)
+  $CancelButton = New-Object System.Windows.Forms.Button
+  $CancelButton.Location = New-Object System.Drawing.Point(150,120)
+  $CancelButton.Size = New-Object System.Drawing.Size(75,23)
+  $CancelButton.Text = 'Cancel'
+  $CancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+  $form.CancelButton = $CancelButton
+  $form.Controls.Add($CancelButton)
 
-$listBox = New-Object System.Windows.Forms.Listbox
-$listBox.Location = New-Object System.Drawing.Point(10,40)
-$listBox.Size = New-Object System.Drawing.Size(260,20)
+  $label = New-Object System.Windows.Forms.Label
+  $label.Location = New-Object System.Drawing.Point(10,20)
+  $label.Size = New-Object System.Drawing.Size(280,20)
+  $label.Text = 'Please make a selection from the list below:'
+  $form.Controls.Add($label)
 
-$listBox.SelectionMode = 'One'
-$listBox.DisplayMember = 'Name'
-$listBox.Height = 70
-$device_list = Get-AudioDevice -List
+  $listBox = New-Object System.Windows.Forms.Listbox
+  $listBox.Location = New-Object System.Drawing.Point(10,40)
+  $listBox.Size = New-Object System.Drawing.Size(260,20)
 
-foreach ($device in $device_list) {
-  if ($device.Type -eq "Playback") {
-    [void] $listBox.Items.Add([PSCustomObject]@{
-      Name = $device.Name
-      AudioDevice = $device
-    })
+  $listBox.SelectionMode = 'One'
+  $listBox.DisplayMember = 'Name'
+  $listBox.Height = 70
+
+  foreach ($device in $AudioDevices) {
+    if ($device.Type -eq "Playback") {
+      [void] $listBox.Items.Add([PSCustomObject]@{
+        Name = $device.Name
+        AudioDevice = $device
+      })
+    }
+  }
+
+  $form.Controls.Add($listBox)
+  $form.Topmost = $true
+
+  $result = $form.ShowDialog()
+
+  if ($result -eq [System.Windows.Forms.DialogResult]::OK -and $listBox.SelectedItem) {
+    return $listBox.SelectedItem.AudioDevice
   }
 }
 
-$form.Controls.Add($listBox)
-$form.Topmost = $true
+$device_list = Get-AudioDevice -List
+$selectedDevice = Select-AudioDevice $device_list
 
-$result = $form.ShowDialog()
-
-if ($result -eq [System.Windows.Forms.DialogResult]::OK -and $listBox.SelectedItem) {
-  Set-AudioDevice $listBox.SelectedItem.AudioDevice.ID -DefaultOnly
-  
+if ($selectedDevice) {
+  Set-AudioDevice $selectedDevice.ID -DefaultOnly
 }
